5ac09b422e409e96a75a06b9a4902415b1a0a017
johwon <wwalxxn@naver.com>
2025-01-06 16:23:32 +0900

    초기 테스트

---
+++ DevProject/src/main/java/com/kh/domain/Board.java
@@ -0,0 +1,28 @@
+package com.kh.domain;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+//@Getter
+//@Setter
+//@NoArgsConstructor	//디생
+//@RequiredArgsConstructor
+//@ToString
+//@EqualsAndHashCode(of="boardNo")
+@Data
+public class Board {
+	private int boardNo;
+	@NonNull
+	private String title;
+	private String content;
+	private Date regDate;
+}
