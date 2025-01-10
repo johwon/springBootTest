fea4d16a0540e70f35a5e86f9a4040cdc1432068
johwon <wwalxxn@naver.com>
2025-01-10 10:09:49 +0900

    @Vaildated 입력값 검증 테스트

--- DevProject/src/main/java/com/kh/controller/MemberController.java
+++ DevProject/src/main/java/com/kh/controller/MemberController.java
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -136,5 +138,65 @@ public class MemberController {
 		log.info("member.getEmail() = " + member.getEmail());
 
 		return "errorsResult";
+	}
+
+	// 입력값 검증을 할 도메인 클래스에 @Vaildated를 지정한다.
+	@RequestMapping(value = "/registerValidation", method = RequestMethod.POST)
+	public String registerValidation(@Validated Member member, BindingResult result) {
+		log.info("registerValidation");
+		if (result.hasErrors()) {
+			return "registerValidationForm"; // 뷰 파일명
+		}
+		log.info("member.getUserId() = " + member.getUserId());
+		log.info("member.getUserName() = " + member.getUserName());
+		log.info("member.getGender() = " + member.getGender());
+		return "success";
+	}
+
+	@RequestMapping(value = "/registerValidationForm01", method = RequestMethod.GET)
+	public String registerForm01(Model model) {
+		log.info("registerValidationForm01");
+		model.addAttribute("member", new Member());
+		return "registerValidationForm"; // 뷰 파일명
+	}
+
+	// 전체적인 입력값 검증
+	@RequestMapping(value = "/registerValidation2", method = RequestMethod.POST)
+	public String registerValidation2(@Validated Member member, BindingResult result) {
+		log.info("registerValidation2");
+		// 입력값 검증 에러가 발생한 경우 true 를 반환한다.
+		log.info("result.hasErrors() = " + result.hasErrors());
+		// 입력값 검증 후 BindingResult 가 제공하는 메서드를 이용하여 검사 결과를 확인한다.
+		if (result.hasErrors()) {
+			List<ObjectError> allErrors = result.getAllErrors();
+			List<ObjectError> globalErrors = result.getGlobalErrors();
+			List<FieldError> fieldErrors = result.getFieldErrors();
+			log.info("allErrors.size() = " + allErrors.size());
+			log.info("globalErrors.size() = " + globalErrors.size());
+			log.info("fieldErrors.size() = " + fieldErrors.size());
+			for (int i = 0; i < allErrors.size(); i++) {
+				ObjectError objectError = allErrors.get(i);
+				log.info("allError = " + objectError);
+			}
+			for (int i = 0; i < globalErrors.size(); i++) {
+				ObjectError objectError = globalErrors.get(i);
+				log.info("globalError = " + objectError);
+			}
+			for (int i = 0; i < fieldErrors.size(); i++) {
+				FieldError fieldError = fieldErrors.get(i);
+				log.info("fieldError.getDefaultMessage() = " + fieldError.getDefaultMessage());
+			}
+			return "registerValidation2Form"; // 뷰 파일명
+		}
+		log.info("member.getUserId() = " + member.getUserId());
+		log.info("member.getGender() = " + member.getGender());
+		return "home";
+	}
+
+	@RequestMapping(value = "/registerValidation2Form01", method = RequestMethod.GET)
+	public String registerValidation2Form01(Model model) {
+		log.info("registerValidation2Form01");
+		model.addAttribute("member", new Member());
+		return "registerValidation2Form"; // 뷰 파일명
 	}
 }
--- DevProject/src/main/java/com/kh/domain/Member.java
+++ DevProject/src/main/java/com/kh/domain/Member.java
@@ -4,17 +4,29 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Member {
 	@NotBlank(message="공백이나 빈칸일 수 없습니다.")
 	private String userId;
+	@NotBlank
 	private String password;
+	@Email
 	private String email;
+	@NotBlank(message="공백이나 빈칸일 수 없습니다.")
+	@Size(max=3)
 	private String userName;
 	private int coin;
-	private Date dateOfBirth;
+	@Past
+	@DateTimeFormat(pattern = "yyyy-MM-dd") 
+	private Date dateOfBirth; 
 	private List<String> hobbyList;
+	private String gender; 
 }
