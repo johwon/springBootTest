6faa280f68c96323784d67e9bc1a3a1b4cf2ce9f
johwon <wwalxxn@naver.com>
2025-01-14 12:44:31 +0900

    mybatis board 검색 기능 테스트

--- SpringMybatisProject/src/main/java/com/zeus/controller/BoardController.java
+++ SpringMybatisProject/src/main/java/com/zeus/controller/BoardController.java
@@ -22,13 +22,24 @@ public class BoardController {
 	@Autowired
 	private BoardService service;
 
-	//게시판 입력폼 요청(/WEB-INF/views/board/register.jsp)
+	@RequestMapping(value = "/search", method = RequestMethod.POST)
+	public String search(String title, Model model) throws Exception {
+		log.info("search");
+		Board board = new Board();
+		board.setTitle(title);
+
+		model.addAttribute("board", board);
+		model.addAttribute("list", service.search(title));
+		return "board/list";
+	}
+
+	// 게시판 입력폼 요청(/WEB-INF/views/board/register.jsp)
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public void registerForm(Board board, Model model) throws Exception {
 		log.info("registerForm");
 	}
 
-	//게시판 입력 내용 저장 후 성공메세지를(/WEB-INF/views/board/success.jsp)
+	// 게시판 입력 내용 저장 후 성공메세지를(/WEB-INF/views/board/success.jsp)
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String register(Board board, Model model) throws Exception {
 		service.register(board);
@@ -36,20 +47,21 @@ public class BoardController {
 		return "board/success";
 	}
 
-	//게시판 전체 리스트를 요청 -> 전체 리스트를 가져와서 (/WEB-INF/views/board/list.jsp)화면에 전달
+	// 게시판 전체 리스트를 요청 -> 전체 리스트를 가져와서 (/WEB-INF/views/board/list.jsp)화면에 전달
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public void list(Model model) throws Exception {
 		log.info("list");
+		model.addAttribute("board", new Board()); 
 		model.addAttribute("list", service.list());
 	}
-	
-	//게시글 상세내용 요청 -> 한개의 글을 가져와서 (/WEB-INF/views/board/board/read.jsp) 화면에 전달
+
+	// 게시글 상세내용 요청 -> 한개의 글을 가져와서 (/WEB-INF/views/board/board/read.jsp) 화면에 전달
 	@RequestMapping(value = "/read", method = RequestMethod.GET)
 	public void read(@RequestParam("boardNo") int boardNo, Model model) throws Exception {
 		model.addAttribute(service.read(boardNo));
 	}
 
-	//게시글 삭제 요청 -> 삭제 진행 -> 결과 (/WEB-INF/views/board/success.jsp) 화면에 전달
+	// 게시글 삭제 요청 -> 삭제 진행 -> 결과 (/WEB-INF/views/board/success.jsp) 화면에 전달
 	@RequestMapping(value = "/remove", method = RequestMethod.POST)
 	public String remove(@RequestParam("boardNo") int boardNo, Model model) throws Exception {
 		service.remove(boardNo);
@@ -57,13 +69,13 @@ public class BoardController {
 		return "board/success";
 	}
 
-	//게시글 수정하기 위한 화면 요청 -> 해당된 게시글 가져와서 (/WEB-INF/views/board/modify.jsp) 화면에 전달
+	// 게시글 수정하기 위한 화면 요청 -> 해당된 게시글 가져와서 (/WEB-INF/views/board/modify.jsp) 화면에 전달
 	@RequestMapping(value = "/modify", method = RequestMethod.GET)
 	public void modifyForm(int boardNo, Model model) throws Exception {
 		model.addAttribute(service.read(boardNo));
 	}
 
-	//게시글 수정 내용 저장 -> 결과 (/WEB-INF/views/board/success.jsp) 화면에 전달
+	// 게시글 수정 내용 저장 -> 결과 (/WEB-INF/views/board/success.jsp) 화면에 전달
 	@RequestMapping(value = "/modify", method = RequestMethod.POST)
 	public String modify(Board board, Model model) throws Exception {
 		service.modify(board);
